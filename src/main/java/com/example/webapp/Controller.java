9d9c4f9a98a10665a9a42d7a9fc37252d55a8c7f
Gerberg1 <144436875+Gerberg1@users.noreply.github.com>
2025-01-09 18:17:47 +0100

    Initial commit

---
+++ src/main/java/com/example/webapp/Controller.java
@@ -0,0 +1,13 @@
+package com.example.webapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello";
+    }
+}
